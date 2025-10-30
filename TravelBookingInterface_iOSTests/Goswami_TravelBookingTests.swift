851d4cbc97a5682c8fc4ef320b332f68f3a626c2
Bhavesh Goswami <goswamjbhavesh@gmail.com>
2025-10-30 11:33:54 -0500

    Create Goswami_TravelBookingTests.swift

---
+++ TravelBookingInterface_iOSTests/Goswami_TravelBookingTests.swift
@@ -0,0 +1,17 @@
+//
+//  Goswami_TravelBookingTests.swift
+//  Goswami_TravelBookingTests
+//
+//  Created by Bhavesh on 10/29/25.
+//
+
+import Testing
+@testable import Goswami_TravelBooking
+
+struct Goswami_TravelBookingTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
